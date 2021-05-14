59f2384c861f0210320d27ccc0b3c9e0466e29b7
Vlad Tkachuk <vladtk@cedar5.cedar.computecanada.ca>
2021-05-13 20:38:06 -0700

    fixed typo

--- parallel/lunar_lander_er.jl
+++ parallel/lunar_lander_er.jl
@@ -13,5 +13,4 @@ Pkg.activate(".")
 
 env_submit_dir =  get(ENV, "SLURM_SUBMIT_DIR", pwd())
 include(joinpath(env_submit_dir, "parallel/parallel_config.jl"))
-reproduce_config_experiment("configs/lunar_lander_er.toml";
-save_path="/home/mkschleg/scratch/ActionRNNs")
+reproduce_config_experiment("configs/lunar_lander_er.toml"; save_path="/home/vladtk/scratch/ActionRNNs")
