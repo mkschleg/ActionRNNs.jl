126e295bc8963fdb227fc0f1cd10adf44f8bd133
Volodymyr Tkachuk <tkachuk.v4@gmail.com>
2021-06-24 17:51:48 -0400

    change default_args such that the :successes are different local and on CC

--- experiment/dir_tmaze_online.jl
+++ experiment/dir_tmaze_online.jl
@@ -22,24 +22,40 @@ const FLU = ActionRNNs.FluxUtils
 
 function default_config()
     Dict{String,Any}(
-        "save_dir" => "dir_tmaze_online",
+         "save_dir" => "dir_tmaze_online",
 
-        "seed" => 1,
-        "steps" => 80000,
-        "size" => 6,
+         "seed" => 2,
+         "steps" => 20000,
+         "size" => 10,
 
-        "cell" => "MARNN",
-        "numhidden" => 6,
-        
-        "opt" => "RMSProp",
-        "eta" => 0.0005,
-        "rho" => 0.99,
-        "truncation" => 8,
+         "cell" => "AARNN",
+         "numhidden" => 12,
 
-#         "hs_learnable" => true,
+         "opt" => "RMSProp",
+         "eta" => 0.001,
+         "rho" => 0.99,
+         "truncation" => 12,
 
-        "gamma"=>0.99)
+           "hs_learnable" => true,
 
+         "gamma"=>0.99)
+#         "save_dir" => "dir_tmaze_online",
+#
+#         "seed" => 1,
+#         "steps" => 80000,
+#         "size" => 6,
+#
+#         "cell" => "MARNN",
+#         "numhidden" => 6,
+#
+#         "opt" => "RMSProp",
+#         "eta" => 0.0005,
+#         "rho" => 0.99,
+#         "truncation" => 8,
+#
+# #         "hs_learnable" => true,
+#
+#         "gamma"=>0.99)
 end
 
 function get_ann(parsed, fs, env, rng)
