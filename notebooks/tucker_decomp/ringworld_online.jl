3eeba351c4a222e4452a7b40a71f9782ecae478b
Volodymyr Tkachuk <tkachuk.v4@gmail.com>
2021-07-18 09:42:28 -0400

    added tucker decomp notebooks

--- notebooks/tucker_decomp/ringworld_online.jl
+++ notebooks/tucker_decomp/ringworld_online.jl
@@ -38,9 +38,13 @@ cell_colors = Dict(
 # ╔═╡ a95b8a7d-9ddd-4e24-b6bb-7557016c033d
 push!(RPU.stats_plot_types, :dotplot)
 
+# ╔═╡ 2c7e6239-b59f-4e8c-9762-c041ad51547c
+ic_on, dd_on = RPU.load_data("../../local_data/tucker_decomp/ringworld_online_rmsprop_10_tuc/")
+
 # ╔═╡ Cell order:
 # ╠═e3d13806-e4e9-11eb-1851-4b51c9ec551c
 # ╠═b70f28c6-9b9c-4abd-9dec-f50dc0b51b90
 # ╟─288917c9-24ca-41f9-854f-6ad3e805a7ad
 # ╟─8698a6e7-3832-4455-8177-94ccf44b09cc
 # ╠═a95b8a7d-9ddd-4e24-b6bb-7557016c033d
+# ╠═2c7e6239-b59f-4e8c-9762-c041ad51547c
