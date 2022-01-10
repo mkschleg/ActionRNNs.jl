d216134020db9a5657eda57332684535d3327e21
Matthew Schlegel <mkschleg@gmail.com>
2022-01-10 12:49:15 -0700

    New combo cell for RNNs.

--- src/rnns/RNNCombo.jl
+++ src/rnns/RNNCombo.jl
@@ -65,6 +65,7 @@ function (m::CaddRNNCell)(h, x::Tuple{A, X}) where {A, X}
     return new_h, reshape(new_h, :, sz[2:end]...)
 end
 
+
 Flux.@functor CaddRNNCell
 
 function Base.show(io::IO, l::CaddRNNCell)
@@ -157,3 +158,94 @@ output fed back into the input each time step.
 
 CcatRNN(a...; ka...) = Flux.Recur(CcatRNNCell(a...; ka...))
 Flux.Recur(m::CcatRNNCell) = Flux.Recur(m, m.state0)
+
+
+
+
+
+#=
+Element wise add.
+=#
+
+
+struct CaddElRNNCell{F,A,V,T,S} <: AbstractActionRNN
+    σ::F
+    θa::V
+    θm::V
+    Wi::A
+    Wa::A
+    Wha::A
+    ba::V
+    Wx::T
+    Whm::T
+    bm::A
+    state0::S
+end
+
+CaddElRNNCell(in::Integer,
+          na::Integer,
+          out::Integer,
+          σ=tanh;
+          init=Flux.glorot_uniform,
+          initb=Flux.zeros,
+          init_state=Flux.zeros) = 
+              CaddElRNNCell(σ,
+                            # Flux.ones(2),
+                            Flux.ones(out),
+                            Flux.ones(out),
+                            init(out, in),
+                            init(out, na),
+                            init(out, out),
+                            initb(out),
+                            init(na, out, in; ignore_dims=1),
+                            init(na, out, out; ignore_dims=1),
+                            initb(out, na),
+                            init_state(out, 1))
+
+function (m::CaddElRNNCell)(h, x::Tuple{A, X}) where {A, X}
+    σ, θa, θm, Wi, Wa, Wha, ba = m.σ, m.θa, m.θm, m.Wi, m.Wa, m.Wha, m.ba
+    Wx, Whm, bm = m.Wx, m.Whm, m.bm
+
+    o = x[2]
+    a = x[1]
+
+    # additive
+    new_ha = σ.(Wi*o .+ get_waa(Wa, a) .+ Wha*h .+ ba)
+
+    # multiplicative
+    wx = contract_WA(m.Wx, a, o)
+    wh = contract_WA(m.Whm, a, h)
+    ba = get_waa(m.bm, a)
+
+    new_hm = σ.(wx .+ wh .+ ba)
+    
+    if new_hm isa AbstractVector
+        new_hm = reshape(new_hm, :, 1)
+    end
+
+    # adding together state
+    # new_h = (w[1]*new_ha + w[2]*new_hm) ./ sum(w)
+    new_h = (θa .* new_ha .+ θm .* new_hm) ./ (θa .+ θm)
+    
+
+    sz = size(o)
+    return new_h, reshape(new_h, :, sz[2:end]...)
+end
+
+
+Flux.@functor CaddElRNNCell
+
+function Base.show(io::IO, l::CaddElRNNCell)
+  print(io, "CaddElRNNCell(", size(l.Wi, 2), ", ", size(l.Wa), ", ", size(l.Wi, 1))
+  l.σ == identity || print(io, ", ", l.σ)
+  print(io, ")")
+end
+
+"""
+    CaddElRNN(in::Integer, out::Integer, σ = tanh)
+The most basic recurrent layer; essentially acts as a `Dense` layer, but with the
+output fed back into the input each time step.
+"""
+
+CaddElRNN(a...; ka...) = Flux.Recur(CaddElRNNCell(a...; ka...))
+Flux.Recur(m::CaddElRNNCell) = Flux.Recur(m, m.state0)
