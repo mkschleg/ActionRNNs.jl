708dab6a2802d0fa853482d2cc030c35dd0a5591
Matthew Schlegel <mkschleg@gmail.com>
2022-01-14 13:21:20 -0700

    Deprecated three arg experiment_wrapper.

--- src/exp_util/experiment.jl
+++ src/exp_util/experiment.jl
@@ -7,6 +7,7 @@ using FileIO
 import Reproduce
 
 experiment_wrapper(exp_func, parsed; hash_exclude_save_dir=false, kwargs...) = Reproduce.experiment_wrapper(exp_func, parsed; hash_exclude_save_dir=hash_exclude_save_dir, kwargs...)
-experiment_wrapper(exp_func::Function, parsed, working::Bool; kwargs...) =
-    experiment_wrapper(exp_func, parsed; working=working, kwargs...)
-
+experiment_wrapper(exp_func::Function, parsed, working::Bool; kwargs...) = begin
+    @warn "Working in experiment wrapper is deprecated." maxlog=1
+    experiment_wrapper(exp_func, parsed; kwargs...)
+end
