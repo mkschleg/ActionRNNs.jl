135e28814cb1d987602d06be2ec0b078323afc1a
Matthew Schlegel <mkschleg@gmail.com>
2022-01-17 15:51:46 -0700

    Working on docs.

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,20 @@
 # ActionRNNs.jl
 
 Documentation for ActionRNNs.jl
+
+## Cells
+
+```@docs
+AARNN
+```
+
+### Shared operations
+
+```@docs
+ActionRNNs.get_waa
+```
+
+## Agents
+
+## Misc Stuff
+
--- src/RNNs.jl
+++ src/RNNs.jl
@@ -25,7 +25,8 @@ _needs_action_input(m::AbstractActionRNN) = true
 """
     contract_WA(W, a::Int, x)
     contract_WA(W, a::AbstractVector{Int}, x)
-    contract_WA(W::CuArray, a::Vector{Int}, x)
+    contract_WA(W, a::AbstractVector{<:AbstractFloat}, x)
+    contract_WA(W::CuArray, a::AbstractVector{Int}, x)
 
 This contraction operator will take the weights `W`, action (or action vector for batches) `a`, and features.
 The weight matrix is assumed to be in `nactions × out × in`.
@@ -42,12 +43,6 @@ function contract_WA(W, a::AbstractVector{Int}, x)
     @tullio ret[i, k] := mid[a[k], i, k]
 end
 
-# Maybe fixed by new version of tullio.
-function contract_WA(W::CuArray, a::AbstractVector{Int}, x)
-    mid = @view (W ⊡ x)[a, :, :]
-    @tullio ret[i, k] := mid[k, i, k]
-end
-
 function contract_WA(W, a::AbstractVector{Int}, x::AbstractVector)
     @tullio ret[i, k] := W[a[k], i, 1] * x[k] # if state is scalar?
 end
@@ -60,13 +55,28 @@ function contract_WA(W, a::AbstractMatrix{<:AbstractFloat}, x)
     @tullio ret[i, k] := W[l, i, j] * a[l, k] * x[j, k]
 end
 
+# GPU Versions
+# Maybe fixed by new version of tullio.
+function contract_WA(W::CuArray, a::AbstractVector{Int}, x)
+    mid = @view (W ⊡ x)[a, :, :]
+    @tullio ret[i, k] := mid[k, i, k]
+end
+
+
 # Precompile commands to reduce latency.
 precompile(contract_WA, (Matrix{Float32}, Vector{Int}, Matrix{Float32}))
 precompile(contract_WA, (Matrix{Float32}, Int, Vector{Float32}))
 precompile(contract_WA, (Matrix{Float32}, Int, Matrix{Float32}))
 
 
+"""
+    get_waa(Wa, a)
 
+Different ways of handeling geting action value from a set of weights. This operation
+can be seen as `Wa*a` where `Wa` is the weight matrix, and a is the action representation.
+This is to be used with various cells to incorporate this operation more reliably.
+
+"""
 get_waa(Wa, a::Int) = Wa[:, a]
 get_waa(Wa, a::Vector{Int}) = Wa[:, a]
 get_waa(Wa, a::AbstractArray{<:AbstractFloat}) = Wa*a
